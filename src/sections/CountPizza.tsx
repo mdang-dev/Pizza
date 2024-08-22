7df2af1a9ba04a7b28891bf0572541ce0686a93c
mdang-dev <minhdang25.dev@gmail.com>
2024-08-22 22:37:47 +0700

    add_code_useEffect_and_ct_btn

---
+++ src/sections/CountPizza.tsx
@@ -0,0 +1,22 @@
+import { useEffect } from "react";
+
+type Props = {
+    count: number,
+    setCount: (count: number) => void;
+};
+
+const CountPizza = ({
+    count,
+    setCount
+}: Props) => {
+
+    useEffect(() => {
+        setCount(0);
+    }, []);
+
+    return (
+        <button onClick={() => setCount(count + 1)}>Increase</button>
+    );
+};
+
+export default CountPizza;
