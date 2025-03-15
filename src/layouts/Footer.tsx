895507d2f0828d40dc4847cf6ad0dbe9620ca161
Miyatoshi007 <154341775+Miyatoshi007@users.noreply.github.com>
2025-03-15 19:43:55 +0700

    Update Footer.tsx

--- src/layouts/Footer.tsx
+++ src/layouts/Footer.tsx
@@ -1,7 +1,7 @@
 const Footer = () => {
     return (
         <div className="bg-slate-200 w-[100%] text-center py-5 font-medium text-lg z-50 bottom-0 sticky top-[90%] mt-10">
-            Copyright 2024 - Dag Dev
+            Copyright 2024 - Dag Dev (text)
         </div>
     );
 };
